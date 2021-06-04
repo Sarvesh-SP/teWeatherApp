020f235ccf514980d8b66066e48b09daa107cd59
Sarvesh-SP <sarvesh.sp2013@gmail.com>
2021-06-04 19:25:47 +0530

    linked to app.js

--- src/server.js
+++ src/server.js
@@ -61,7 +61,6 @@ app.get('/weather', (req, res) => {
           if (error) {
             return res.send(error)
           } else {
-            
             return res.send({
               foreCast: des,
               temperature: temp,
